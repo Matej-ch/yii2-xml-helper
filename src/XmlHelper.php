0f92c16dc34e8777a0169dc4986db9accc779792
Matej <matejchalachan@gmail.com>
2021-06-09 09:58:43 +0200

    updated error handling of xml file reading

--- src/XmlHelper.php
+++ src/XmlHelper.php
@@ -3,7 +3,7 @@
 namespace matejch\xmlhelper;
 
 /**
- * This is just an example.
+ * Loading xml file
  */
 class XmlHelper extends \yii\base\BaseObject
 {
@@ -19,21 +19,50 @@ class XmlHelper extends \yii\base\BaseObject
 
         $result = simplexml_load_string($rawBody, 'SimpleXMLElement', LIBXML_NOCDATA);
 
-        if($result ===false)
-        {
+        if($result === false) {
             $errors = libxml_get_errors();
-            $latestError = array_pop($errors);
+
+            $msg = '';
+            foreach ($errors as $error) {
+                $msg .= $this->parseError($error);
+            }
+
+            libxml_clear_errors();
+
             return [
-                'message' => $latestError->message,
-                'type' => $latestError->level,
-                'code' => $latestError->code,
-                'file' => $latestError->file,
-                'line' => $latestError->line,
+                'message' => $msg ?? 'Error message not found',
             ];
         }
+
         if (!$this->asArray) {
             return $result;
         }
         return json_decode(json_encode($result), true);
+    }
+
+    private function parseError($error): string
+    {
+        $message = '';
+        switch ($error->level) {
+            case LIBXML_ERR_WARNING:
+                $message .= "Warning $error->code: ";
+                break;
+            case LIBXML_ERR_ERROR:
+                $message .= "Error $error->code: ";
+                break;
+            case LIBXML_ERR_FATAL:
+                $message .= "Fatal Error $error->code: ";
+                break;
+        }
+
+        $message .= trim($error->message) .
+            "\n  Line: $error->line" .
+            "\n  Column: $error->column";
+
+        if ($error->file) {
+            $message .= "\n  File: $error->file";
+        }
+
+        return $message."\n";
     }
 }
